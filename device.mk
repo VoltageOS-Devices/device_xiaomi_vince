8e63b650b658032de90ddf315b9c3a24e6bb9952
GhostMaster69-dev <rathore6375@gmail.com>
2022-05-29 07:26:06 +0530

    vince: Drop Snap overlays
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- device.mk
+++ device.mk
@@ -40,7 +40,6 @@ PRODUCT_ENFORCE_RRO_TARGETS := \
 
 # RRO (Runtime Resource Overlay)
 PRODUCT_ENFORCE_RRO_TARGETS := *
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/Snap
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
